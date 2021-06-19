4512d49165371de84088c5a95b8de080fa0f306e
First <natthasit.pan@innovasive.co.th>
2021-06-19 16:58:25 +0700

    👾 add Navigator in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 // import { useRouter } from 'next/router'
 
+import Navigator from '../core/components/Navigator'
+import { MENU } from '../core/constant/constant'
 
 const Home = () => {
   // const router = useRouter()
 
   return (
     <div className="">
-      Hello
+      <Navigator currentPage={MENU.MENU_HOME}>
+        <>
+          Hello
+        </>
+      </Navigator>
     </div>
   )
 }
