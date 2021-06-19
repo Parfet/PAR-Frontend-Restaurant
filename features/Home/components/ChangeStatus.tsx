5cb0e4a34db2ca94c0f81872369bcde549625ed2
First <natthasit.pan@innovasive.co.th>
2021-06-19 18:50:02 +0700

    👾 add Change Restauranr Status

---
+++ features/Home/components/ChangeStatus.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { useFormik } from 'formik';
+import _ from 'lodash'
+
+import { RestaurantStatus } from '../../../core/constant/enum'
+import { RegularText } from '../../../core/config/textStyle'
+
+const optionItem = [
+  {
+    label: 'เปิดร้าน',
+    value: RestaurantStatus.RESTAURANT_OPEN,
+  },
+  {
+    label: 'ปิดร้าน',
+    value: RestaurantStatus.RESTAURANT_CLOSED
+  }
+]
+
+const ChangeStatus = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      restaurantStatus: RestaurantStatus.RESTAURANT_OPEN
+    },
+    onSubmit: (values) => {
+      console.log(values.restaurantStatus)
+    },
+  });
+
+  return (
+    <div className="bg-white rounded-25 p-16">
+      <div className="text-center">
+        <RegularText>สถานะร้านบุฟเฟต์</RegularText>
+      </div>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="inline-block relative w-64 my-3">
+          <select 
+            id="restaurantStatus"
+            name="restaurantStatus"
+            onChange={formik.handleChange}
+            className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline rounded-25"
+            >
+            {
+              _.map(optionItem, (data, index) => (
+                <option value={data.value} key={index}>{data.label}</option>
+              ))
+            }
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <button className="bg-cusYellow hover:bg-cusYellow-500 text-white font-bold py-2 px-4 rounded-25" type="submit">
+            <RegularText>ยืนยัน</RegularText>
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+
+}
+
+export default ChangeStatus
---
+++ features/Home/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import ChangeStatus from '../components/ChangeStatus'
+
+const DashBoard = () => {
+
+  return (
+    <div className="flex justify-center flex-wrap content-center w-full">
+      <ChangeStatus />
+    </div>
+  )
+
+}
+
+export default DashBoard
