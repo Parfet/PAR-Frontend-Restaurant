923623750de689cc005d84777d2e4e0e2e5b8ab2
First <natthasit.pan@innovasive.co.th>
2021-06-19 17:01:00 +0700

    👾 edit remove unused meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,6 @@ class MyDocument extends Document {
           <title>Restaurant</title>
           <meta charSet='utf-8' />
           <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover' />
-          <link rel='icon' type='image/png' href='/images/favicon.png' />
         </Head>
         <body>
           <Main />
