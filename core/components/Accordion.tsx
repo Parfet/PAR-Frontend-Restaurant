e9da9dc8926597ffd43373ec243efe0ebd97e308
First <natthasit.pan@innovasive.co.th>
2021-06-25 00:43:32 +0700

    👾 add action area in Accordion

--- core/components/Accordion.tsx
+++ core/components/Accordion.tsx
@@ -2,12 +2,15 @@ import React, { useRef, useState } from 'react'
 import { TiArrowSortedDown } from "react-icons/ti";
 
 interface Props {
+  classAtButton: string
+  classAtContent: string
   title: JSX.Element
-  content: React.ReactNode
+  content: JSX.Element
+  action?: JSX.Element
 }
 
 const Accordion = (props: Props) => {
-  const { title, content } = props
+  const { title, content, action, classAtButton, classAtContent } = props
   const [active, setActive] = useState(false)
   const [height, setHeight] = useState('0px')
   const [rotate, setRotate] = useState('transform duration-700 ease')
@@ -18,24 +21,33 @@ const Accordion = (props: Props) => {
     setActive(active === false ? true : false)
     // @ts-ignore
     setHeight(active ? '0px' : `${contentSpace.current.scrollHeight}px`)
-    setRotate(active ? 'transform duration-700 ease' : 'transform duration-700 ease rotate-180')
+    setRotate(active ? 'transform duration-700 ease' : 'transform duration-300 ease rotate-180')
   }
 
   return (
     <div className="flex flex-col">
-      <button
-        className="py-2 box-border appearance-none cursor-pointer focus:outline-none flex items-center justify-between"
+      <div className="flex flex-row w-full">
+      <div
+          className={`${classAtButton} ${action ? 'w-5/6': 'w-full'} py-3 box-border appearance-none cursor-pointer focus:outline-none flex items-center rounded-5 `}
         onClick={toggleAccordion}
       >
-        <p className="inline-block text-footnote light">{title}</p>
-        <TiArrowSortedDown className={`${rotate} inline-block`} />
-      </button>
+        <div className="inline-block text-footnote light w-full mx-5">{title}</div>
+        <TiArrowSortedDown className={`${rotate} inline-block mr-3`} style={{fontSize:"22px"}}/>
+      </div>
+      {
+        action ? 
+          <div className="flex flex-row items-center justify-center w-1/6">
+            {action}
+          </div>
+        : <></>
+      }
+      </div>
       <div
         ref={contentSpace}
         style={{ maxHeight: `${height}` }}
-        className="overflow-auto transition-max-height duration-700 ease-in-out scrollbar-hide"
+        className={`${classAtContent} ${action ? 'w-5/6' : 'w-full'} overflow-auto transition-max-height duration-300 ease-in-out scrollbar-hide rounded-5`}
       >
-        <div className="pb-10">{content}</div>
+        <div>{content}</div>
       </div>
     </div>
   )
