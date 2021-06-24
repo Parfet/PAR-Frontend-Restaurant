56d0cbe961faec9222bd1118816e59819adb4920
First <natthasit.pan@innovasive.co.th>
2021-06-25 00:46:24 +0700

    👾 add PromotionStatus

--- core/constant/enum.ts
+++ core/constant/enum.ts
@@ -2,3 +2,8 @@ export enum RestaurantStatus {
   RESTAURANT_OPEN = "OPEN",
   RESTAURANT_CLOSED = "CLOSED"
 }
+
+export enum PromotionStatus {
+  PROMOTION_ACTIVE = "ACTIVE",
+  PROMOTION_INACTIVE = "INACTIVE"
+}
